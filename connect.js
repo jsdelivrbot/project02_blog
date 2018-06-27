64b8048a3b007f21b5633c724ddbfdae22c82cd5
Bryan Lopez <lop00003@byui.edu>
2018-06-27 14:21:42 -0700

    changes in connect.js. I'm putting in the code from index.js since that seems to work.

--- connect.js
+++ connect.js
@@ -1,3 +1,35 @@
+const express = require('express')
+const PORT = process.env.PORT || 5000
+
+const { Client } = require('pg')
+
+express()
+  .get('/handle_get_getPerson', (req, res) => handle_get_getPerson(req, res))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+function handle_get_getPerson(req, res)
+{
+  console.log('We wuz here')
+
+  const client = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: true
+  })
+
+  client.connect()
+
+  client.query('SELECT id, first_name, last_name FROM member WHERE id = $1', [req.query.id], (dberr, dbres) => {
+    if (dberr) throw dberr
+    for (let row of dbres.rows) {
+      res.json(row)
+      console.log(JSON.stringify(row))
+    }
+    client.end()
+  })
+}
+
+
+
 // var express = require('express');
 // var app = express();
 
