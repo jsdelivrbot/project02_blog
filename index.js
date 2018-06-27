761dc8cffb43ac0e8f05d6ef0d54c84e160a5adc
Bryan Lopez <lop00003@byui.edu>
2018-06-27 08:16:51 -0700

    changes to index.js

--- index.js
+++ index.js
@@ -9,22 +9,24 @@ express()
 
 function handle_get_getPerson(req, res)
 {
-  const client = new Client({
-    connectionString: process.env.DATABASE_URL,
-    ssl: true
-  })
+	console.log('We wuz here')
 
-  client.connect()
+  // const client = new Client({
+  //   connectionString: process.env.DATABASE_URL,
+  //   ssl: true
+  }
 
-  client.query('SELECT first_name, last_name FROM member WHERE id = $1', [req.query.id], (dberr, dbres) => {
-    if (dberr) throw dberr
-    for (let row of dbres.rows) {
-      res.json(row)
-      console.log(JSON.stringify(row))
-    }
-    client.end()
-  })
-}
+//   client.connect()
+
+//   client.query('SELECT first_name, last_name FROM member WHERE id = $1', [req.query.id], (dberr, dbres) => {
+//     if (dberr) throw dberr
+//     for (let row of dbres.rows) {
+//       res.json(row)
+//       console.log(JSON.stringify(row))
+//     }
+//     client.end()
+//   })
+// }
 
 
 // var express = require('express');
