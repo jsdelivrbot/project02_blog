fcb62498a9de96c4ae8f22938cd1265874e02d4f
Bryan Lopez <lop00003@byui.edu>
2018-06-27 13:00:12 -0700

    I'm trying to figure out if I messed up the npm, so I'm going to upload the teach assignment and see if it works.

---
+++ public/teach10.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const PORT = process.env.PORT || 5000
+
+const { Client } = require('pg')
+
+express()
+  .get('/getPerson', (req, res) => handle_get_getPerson(req, res))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+function handle_get_getPerson(req, res)
+{
+  const client = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: true
+  })
+
+  client.connect()
+
+  client.query('SELECT first_name, last_name, date_of_birth FROM toa10_person WHERE id = $1', [req.query.id], (dberr, dbres) => {
+    if (dberr) throw dberr
+    for (let row of dbres.rows) {
+      res.json(row)
+      // console.log(JSON.stringify(row))
+    }
+    client.end()
+  })
+}
