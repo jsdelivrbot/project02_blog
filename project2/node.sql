cdcb08b3cfa7fde73eb004fa76f61d931dbbd56c
Bryan Lopez <lop00003@byui.edu>
2018-06-26 22:27:37 -0700

    changes to sql

--- project2/node.sql
+++ project2/node.sql
@@ -1,5 +1,15 @@
-CREATE TABLE member (
-   id			SERIAL PRIMARY KEY NOT NULL,
+CREATE TABLE member(
+   id			SERIAL PRIMARY KEY,
    first_name	TEXT NOT NULL,
    last_name	TEXT NOT NULL
 );
+
+CREATE TABLE login(
+   login_id    SERIAL  PRIMARY KEY,
+   member_id   INTEGER REFERENCES member(id),
+   username	   text    NOT NULL,
+   password    text    NOT NULL 
+);
+
+INSERT INTO member(first_name, last_name) VALUES
+ ('Bryan',  'Lopez');
