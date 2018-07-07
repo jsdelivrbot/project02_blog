918cbb154c0e03f8b4929cc9e62cf07ff070ce82
Bryan Lopez <lop00003@byui.edu>
2018-07-06 17:23:25 -0700

    changing path

--- connect.js
+++ connect.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const PORT = process.env.PORT || 5000
-var groupHelp = require('groupHelp')
+const groupHelp = require(' /view/groupHelp.ejs')
 const { Client } = require('pg')
 
 express()
