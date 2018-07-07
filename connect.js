1a9dc3ef3e248752f32040175ab9b3e4fa160eed
Bryan Lopez <lop00003@byui.edu>
2018-07-07 12:07:43 -0700

    debug

--- connect.js
+++ connect.js
@@ -7,7 +7,7 @@ express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/connect'))
+  .get('/', (req, res) => res.render('pages/index'))
   
   .use('/view/groupHelp', groupHelp)
 
