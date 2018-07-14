a8dcd8a4a2105c1a3fec597eb11388a6425e8083
Bryan Lopez <lop00003@byui.edu>
2018-07-14 13:17:38 -0700

    starting over

--- connect.js
+++ connect.js
@@ -1,29 +1,60 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
-var groupHelp = require('/groupHelp')
+const bodyParser = require('body-parser');
+const session = require('express-session');
+var app = express();
 
 
-const { Pool } = require("pg");
-const pool = new Pool({connectionString: connectionString});
 
 
+// const express = require('express');
+// const path = require('path');
+// var app = express();
+// const { Client } = require('pg')
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-   .get('/view/groupHelp', groupHelp)
+// var session = require('express-session');
 
-  .get("/getPerson", getPerson);
-  .get("/getComment", getComment);
-  .get("/getNewItem", getNewItem);
-  .get("/getFinishedItem", getFinishedItem);
+// app.use(session({
+//   secret: 'my-super-secret-secret!',
+//   resave: false,
+//   saveUninitialized: true
+// }))
 
-  .get('/groupHelp', function (req, res) {
-  res.render('pages/groupHelp', {
+// var bodyParser = require('body-parser')
+// app.use( bodyParser.json() );       // to support JSON-encoded bodies
+// app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+//   extended: true
+// }));
 
-  })
-})
-  
+// app.set('port', (process.env.PORT || 5000));
+
+// app.use(express.static(path.join(__dirname, 'public')));
+
+// app.use(logRequest);
+
+// // Setting up the routes
+// app.post('/login', handleLogin);
+// app.post('/logout', handleLogout);
+
+// app.get('/getServerTime', verifyLogin, getServerTime);
+
+// app.listen(app.get('port'), function() {
+//   console.log('Node app is running on port', app.get('port'));
+// });
+
+const addToList = require('./addToList.js');
+
+  app.use(express.static(path.join(__dirname, 'public')))
+  app.use(bodyParser.json())
+  app.use(bodyParser.urlencoded({ extended: true }))
+  app.use(session({
+  secret: 'this-is-my-secret',
+  resave: false,
+  saveUninitialized: true
+}))
+  app.use('/addToList', addToList.getRoute())
+  app.set('views', path.join(__dirname, 'views'))
+  app.set('view engine', 'ejs')
+  app.get('/', (req, res) => res.render('pages/index'))
+  app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
