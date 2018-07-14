efa9ee1230ccf87892f0e4514731dc39fb5627e1
Bryan Lopez <lop00003@byui.edu>
2018-07-14 13:56:58 -0700

    still trying to figure things out

--- public/index.js
+++ public/index.js
@@ -38,6 +38,85 @@
 //   });
 // }
 
+function addTheComment(addComment, callback) {
+  console.log('saving comment: ', addComment);
+
+  var sql = 'INSERT INTO comments(comments) VALUES ($1)';
+
+  var params = [addComment.comments];
+
+  dbconnect.query(sql, params, function(error, result) {
+    if (error) {
+      console.log('A DB error occured');
+      console.log(error);
+    }
+
+    console.log('Inserted into DB');
+
+    //callback(null, vip.js/1);
+  });
+}
+
+
+// Checks if the username and password match a hardcoded set
+// If they do, put the username on the session
+function handleLogin(request, response) {
+  var result = {success: false};
+
+  // We should do better error checking here to make sure the parameters are present
+  if (request.body.username == "admin" && request.body.password == "password") {
+    request.session.user = request.body.username;
+    result = {success: true};
+  }
+
+  response.json(result);
+}
+
+// If a user is currently stored on the session, removes it
+function handleLogout(request, response) {
+  var result = {success: false};
+
+  // We should do better error checking here to make sure the parameters are present
+  if (request.session.user) {
+    request.session.destroy();
+    result = {success: true};
+  }
+
+  response.json(result);
+}
+
+// This function returns the current server time
+function getServerTime(request, response) {
+  var time = new Date();
+  
+  var result = {success: true, time: time};
+  response.json(result); 
+}
+
+// This is a middleware function that we can use with any request
+// to make sure the user is logged in.
+function verifyLogin(request, response, next) {
+  if (request.session.user) {
+    // They are logged in!
+
+    // pass things along to the next function
+    next();
+  } else {
+    // They are not logged in
+    // Send back an unauthorized status
+    var result = {succes:false, message: "Access Denied"};
+    response.status(401).json(result);
+  }
+}
+
+// This middleware function simply logs the current request to the server
+function logRequest(request, response, next) {
+  console.log("Received a request for: " + request.url);
+
+  // don't forget to call next() to allow the next parts of the pipeline to function
+  next();
+}
+
 
 
     function getPerson(req, res) {
