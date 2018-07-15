790b2d3fa0276387acbe18b8eda8b6dfa887fbdd
Bryan Lopez <lop00003@byui.edu>
2018-07-14 19:34:13 -0700

    adding routes

--- connect.js
+++ connect.js
@@ -34,6 +34,13 @@ app.use(logRequest);
 app.post('/storeComment', addTheComment);
 app.post('/login', handleLogin);
 app.post('/logout', handleLogout);
+//write functions for these
+app.post('/additem', addItemToDb);
+app.get('/getPerson', getPersonFromDb);
+app.get('/getComment', getPersonFromDb);
+app.get('/getNewItem', getNewItemFromDb);
+app.get('/getFinishedItem', getNewItemFromDb);
+
 
 // This method has a middleware function "verifyLogin" that will be called first
 app.get('/getServerTime', verifyLogin, getServerTime);
@@ -129,28 +136,199 @@ function logRequest(request, response, next) {
   next();
 }
 
+// adding my other functions here
+  function getPerson(req, res) {
+  console.log("Getting person information.");
 
-// const express = require('express')
-// const path = require('path')
-// const PORT = process.env.PORT || 5000
-// const bodyParser = require('body-parser');
-// const session = require('express-session');
-// const { Client } = require('pg')
-// var app = express();
+//var id = req.params.id; is used for placeholder
+  var id = req.query.id;
 
+  getPersonFromDb(id, function(error, result) {
 
-// //const addToList = require('./addToList.js');
+    if (error || result == null || result.length != 1) {
+      res.status(500).json({success:false, data: error});
+    } else {
+      res.json(result[0]);
+    } 
 
-//   app.use(express.static(path.join(__dirname, 'public')))
-//   app.use(bodyParser.json())
-//   app.use(bodyParser.urlencoded({ extended: true }))
-//   app.use(session({
-//   secret: 'this-is-my-secret',
-//   resave: false,
-//   saveUninitialized: true
-// }))
-//   //app.use('/addToList', addToList.getRoute())
-//   app.set('views', path.join(__dirname, 'views'))
-//   app.set('view engine', 'ejs')
-//   app.get('/', (req, res) => res.render('pages/index'))
-//   app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+   console.log("Back from database", result);
+   res.json(result);
+  });
+
+  // var result = {id: 238, first: "John", last: "Smith"};
+
+  // res.json(result);
+}
+
+function getPersonFromDb(id, callback) {
+  console.log("getPersonFromDb called with id:", id);
+
+  var sql = "SELECT id, first_name, last_name FROM member WHERE id = $1::int";
+
+  var params = [id];
+
+  pool.query(sql, params, function(err, result) {
+      if (err) {
+        console.log("There was an error with the database.");
+        console.log(err);
+        callback(err, null);
+      }
+      console.log("Found DB result: " + JSON.stringify(result.rows));
+
+      callback(null, result.rows);
+  });
+}
+
+function addItemToDb(addItem, callback) {
+  console.log('saving comment: ', addItemToDb);
+
+  var sql = 'INSERT INTO list(new_item) VALUES (?)';
+
+  var params = [addComment.comments];
+
+  dbconnect.query(sql, params, function(error, result) {
+    if (error) {
+      console.log('A DB error occured');
+      console.log(error);
+    }
+
+    console.log('Inserted into DB');
+
+    //callback(null, vip.js/1);
+  });
+}
+
+function getComment(req, res) {
+  console.log("Getting Comments.");
+
+  var id = req.query.id;
+
+  getCommentFromDb(id, function(error, result) {
+
+    if (error || result == null || result.length != 1) {
+      res.status(500).json({success:false, data: error});
+    } else {
+      res.json(result[0]);
+    } 
+
+   console.log("Back from database", result);
+   res.json(result);
+  });
+}
+
+function getCommentFromDb(id, callback) {
+  console.log("getCommentFromDb called with id:", id);
+
+  var sql = "SELECT comment_id, comments FROM comments WHERE id = $1::int AND comments = $2text";
+
+  var params = [id];
+
+  pool.query(sql, params, function(err, result) {
+      if (err) {
+        console.log("There was an error with the database.");
+        console.log(err);
+        callback(err, null);
+      }
+      console.log("Found DB result: " + JSON.stringify(result.rows));
+
+      callback(null, result.rows);
+  });
+}
+
+function getNewItem(req, res) {
+  console.log("Getting New information.");
+
+//var id = req.params.id; is used for placeholder
+  var id = req.query.id;
+
+  getNewItemFromDb(id, function(error, result) {
+
+    if (error || result == null || result.length != 1) {
+      res.status(500).json({success:false, data: error});
+    } else {
+      res.json(result[0]);
+    } 
+
+   console.log("Back from database", result);
+   res.json(result);
+  });
+
+}
+
+function getNewItemFromDb(id, callback) {
+  console.log("getNewItemFromDb called with id:", id);
+
+  var sql = "SELECT id, new_item FROM list WHERE id = $1::int AND new_item = $2::text";
+
+  var params = [id];
+
+  pool.query(sql, params, function(err, result) {
+      if (err) {
+        console.log("There was an error with the database.");
+        console.log(err);
+        callback(err, null);
+      }
+      console.log("Found DB result: " + JSON.stringify(result.rows));
+
+      callback(null, result.rows);
+  });
+}
+
+function getFinishedItem(req, res) {
+  console.log("Getting Finished information.");
+
+//var id = req.params.id; is used for placeholder
+  var id = req.query.id;
+
+  getFinishedItemFromDb(id, function(error, result) {
+
+    if (error || result == null || result.length != 1) {
+      res.status(500).json({success:false, data: error});
+    } else {
+      res.json(result[0]);
+    } 
+
+   console.log("Back from database", result);
+   res.json(result);
+  });
+
+  
+}
+
+function getFinishedItemFromDb(id, callback) {
+  console.log("getFinishedItemFromDb called with id:", id);
+
+  var sql = "SELECT id, finished_item FROM list WHERE id = $1::int AND finished_item = $2::text";
+
+  var params = [id];
+
+  pool.query(sql, params, function(err, result) {
+      if (err) {
+        console.log("There was an error with the database.");
+        console.log(err);
+        callback(err, null);
+      }
+      console.log("Found DB result: " + JSON.stringify(result.rows));
+
+      callback(null, result.rows);
+  });
+}
+
+function storeComment(addComment, callback) {
+  console.log('saving comment: ', addComment);
+
+  var sql = 'INSERT INTO comments(comments) VALUES ($1)';
+
+  var params = [addComment.comments];
+
+  dbconnect.query(sql, params, function(error, result) {
+    if (error) {
+      console.log('A DB error occured');
+      console.log(error);
+    }
+
+    console.log('Inserted into DB');
+
+    //callback(null, vip.js/1);
+  });
+}
